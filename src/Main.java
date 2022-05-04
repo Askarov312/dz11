9ddbee3073dd201cc0eb5936e688abfd5cded74e
Askarov312 <104826888+Askarov312@users.noreply.github.com>
2022-05-04 17:16:22 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.a = 10;
+        triangle.b = 10;
+        triangle.c = 20;
+        Triangle.area(triangle.a, triangle.b, triangle.c);
+
+    }
+}
